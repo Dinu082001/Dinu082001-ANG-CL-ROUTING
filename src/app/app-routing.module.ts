fc0252c192edbc838655a6c8b087a2ccc7dd9ae7
Dinu082001 <dineshchintapalli0818@gmail.com>
2025-01-05 09:13:13 +0530

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ import { ProfileComponent } from './components/profile/profile.component';
 export const routes: Routes = [
   {path: 'feed', component: FeedComponent},
   {path: 'search', component: SearchComponent},
-  //add the 'profile' route
+  { path: 'profile', component: ProfileComponent },//add the 'profile' route
   {path: '**', component: HomeComponent},
 ];
 
